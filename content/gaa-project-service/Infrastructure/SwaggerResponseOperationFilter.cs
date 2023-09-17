6eca0f0a7a6dadd9644cf18994782b6e011ff3a4
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2023-09-17 15:22:42 +0300

    Добавление шаблона gaa-project-service

---
+++ content/gaa-project-service/Infrastructure/SwaggerResponseOperationFilter.cs
@@ -0,0 +1,53 @@
+﻿using Gaa.Project.Service.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Gaa.Project.Service.Infrastructure;
+
+/// <summary>
+/// Swagger core response type filter.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public sealed class SwaggerResponseOperationFilter : IOperationFilter
+{
+    private static readonly IReadOnlyDictionary<int, string> ErrorStatuses = new Dictionary<int, string>
+    {
+        { StatusCodes.Status400BadRequest, "Ошибка в логике приложения, ошибка валидации." },
+        { StatusCodes.Status401Unauthorized, "Пользователь не авторизован." },
+        { StatusCodes.Status403Forbidden, "Доступ к ресурсу запрещенный." },
+        { StatusCodes.Status422UnprocessableEntity, "Внутренняя ошибка сервера." },
+    };
+
+    /// <inheritdoc />
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (typeof(AboutController).Equals(context.MethodInfo.DeclaringType))
+        {
+            return;
+        }
+
+        var mediaType = new OpenApiMediaType
+        {
+            Schema = context.SchemaGenerator.GenerateSchema(typeof(ProblemDetails), context.SchemaRepository),
+        };
+
+        var content = new Dictionary<string, OpenApiMediaType>
+        {
+            { "text/plain", mediaType },
+            { "application/json", mediaType },
+            { "text/json", mediaType },
+        };
+
+        foreach (var status in ErrorStatuses)
+        {
+            var httpCode = status.Key.ToString(CultureInfo.InvariantCulture);
+            if (!operation.Responses.ContainsKey(httpCode))
+            {
+                operation.Responses.Add(httpCode, new OpenApiResponse { Content = content, Description = status.Value, });
+            }
+        }
+    }
+}
---
+++ content/gaa-project-service/Startup.cs
@@ -0,0 +1,130 @@
+﻿using Gaa.Project.Service.Consumers;
+using Gaa.Project.Service.Infrastructure;
+using Gaa.Project.Service.Jobs;
+using Gaa.Project.Service.Security;
+using MassTransit;
+using Quartz;
+using Quartz.AspNetCore;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace Gaa.Project.Service;
+
+/// <summary>
+/// Класс инициализирующий приложение.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public sealed class Startup
+{
+    /// <inheritdoc cref="IConfiguration"/>
+    public IConfiguration Configuration { get; init; }
+
+    /// <summary>
+    /// Перечень сборок приложения.
+    /// </summary>
+    public IEnumerable<Assembly> Assemblies { get; init; }
+
+    /// <summary>
+    /// Инициализация экземпляра класса <see cref="Startup"/>.
+    /// </summary>
+    /// <param name="configuration">Набор свойств конфигурации приложения.</param>
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+        this.Assemblies = new Assembly[]
+        {
+            typeof(ServiceLayer).Assembly,
+        };
+    }
+
+    /// <summary>
+    /// Метод конфигурации сервисов приложения.
+    /// </summary>
+    /// <param name="services">Коллекция сервисов.</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        #region [ Service configuration ]
+
+        #endregion
+
+        #region [ Infrastructure configuration ]
+
+        services.AddScoped<ServiceUser>();
+        services.AddHttpContextAccessor();
+        services.AddWebApiSwaggerDocumentation(this.Assemblies);
+
+        var uriBuilder = new UriBuilder(this.Configuration.GetConnectionString("RabbitMQ"));
+        services.AddMassTransit(configure =>
+        {
+            configure.AddConsumer<SampleConsumer>();
+
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(uriBuilder.Uri, hostConfigurator =>
+                {
+                    hostConfigurator.Username(uriBuilder.UserName);
+                    hostConfigurator.Password(uriBuilder.Password);
+                });
+
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        services.AddQuartz(configure =>
+        {
+            configure.SchedulerId = $"{Program.ServiceName} Scheduler: '{Guid.NewGuid()}'";
+            configure.SchedulerName = $"{Program.ServiceName} Quartz.NET Scheduler";
+
+            configure.AddJob<SampleJob>(
+                SampleJob.Key,
+                jobConfigurator => jobConfigurator.WithDescription("sample background job"));
+
+            configure.AddTrigger(triggerConfigurator => triggerConfigurator
+                .WithIdentity("cron trigger")
+                .ForJob(SampleJob.Key)
+                .WithCronSchedule("00 */2 * * * ?")
+                .WithDescription("cron trigger for sample background job"));
+        });
+
+        services.AddQuartzServer(options =>
+        {
+            options.WaitForJobsToComplete = true;
+        });
+
+        #endregion
+
+        services.AddControllers(configure =>
+        {
+            configure.Filters.Add<ApiExceptionFilter>();
+        });
+        services.AddEndpointsApiExplorer();
+    }
+
+    /// <summary>
+    /// Метод настройки конвейера HTTP-запросов.
+    /// </summary>
+    /// <param name="builder">Строитель приложения.</param>
+    /// <param name="environment">Окружение приложения.</param>
+    public void Configure(IApplicationBuilder builder, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            builder.UseDeveloperExceptionPage();
+        }
+
+        builder.UseRouting();
+        builder.UseAuthorization();
+
+        builder.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/", context =>
+            {
+                context.Response.Redirect("/swagger");
+                return Task.CompletedTask;
+            });
+        });
+
+        builder.UseSwaggerDocumentation();
+    }
+}
